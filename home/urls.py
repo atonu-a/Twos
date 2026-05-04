e4a69efd718192cdf40402c627df9a7e5d918304
Atonu <snigdho1234@gmail.com>
2026-05-04 12:22:04 +0600

    Changed the whole UI

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,16 @@
+# urls.py - Django URL Configuration
+
 from django.urls import path
 from . import views
 
+
 urlpatterns = [
-    path('', views.index, name="index"),
+    path('', views.index, name='index'),
+    path('about/', views.about, name='about'),
+    path('profile/', views.profile, name ='profile'),
+    path('login/', views.login, name ='login'),
+    path('register/', views.register, name ='register'),
+    path('edit/', views.edit, name ='edit'),
+    
+    
 ]
--- home/views.py
+++ home/views.py
@@ -1,8 +1,28 @@
+# views.py - Django Views
+
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from datetime import datetime, timedelta
+
+
 def index(request):
-    return render(request, "base.html")
+
+
     
+    return render(request, 'index.html')
+
+def about(request):
+    return render(request, "about.html")
+
+def profile(request):
+    return render(request, "profile.html")
+
+def login(request):
+    return render(request,"login.html")
+
+def register(request):
+    return render(request,"registration.html")
 
 
-# Create your views here.
+def edit(request):
+    return render(request,"edit_profile.html")
