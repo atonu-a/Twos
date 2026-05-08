f5ae8a1a934a0a880204d3bcfa93813e187f5717
Atonu <susnigdho1234@gmail.com>
2026-05-08 20:16:03 +0600

    Added due date section for future tasks and made the overviewboard dynamic

--- home/models.py
+++ home/models.py
@@ -12,6 +12,7 @@ class Task(models.Model):
     priority = models.CharField(max_length=20, choices=PRIORITY_CHOICES, default="Low")
     is_completed = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
+    due_date = models.DateField(null=True, blank=True)
     
     
     def __anarchy__(self):
--- home/views.py
+++ home/views.py
@@ -3,14 +3,30 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from datetime import datetime, timedelta
+from django.utils import timezone
 from .models import *
 
 
 def index(request):
+    today = timezone.now().date()
+    
     tasks = Task.objects.order_by("-created_at")
+    completed_tasks = tasks.filter(is_completed=False)
+    total_completed_tasks = completed_tasks.count();
+    high_priority_tasks = tasks.filter(priority="High")
+    total_priority_tasks = high_priority_tasks.count()
+    future_tasks = tasks.filter(due_date__gt=today)
+    total_future_tasks = future_tasks.count()
     
     data = {
         'tasks':tasks,
+        'completed_tasks':completed_tasks,
+        'total_completed_tasks' : total_completed_tasks,
+        'high_priority_tasks': high_priority_tasks,
+        'total_priority_tasks' : total_priority_tasks,
+        'future_tasks':future_tasks,
+        'total_future_tasks' : total_future_tasks,
+        
     }
     return render(request, 'index.html', data)
 
@@ -19,7 +35,12 @@ def add_task(request):
     if request.method == "POST":
         name = request.POST.get("task_name")
         priority = request.POST.get("priority")
-        Task.objects.create(name=name, priority=priority)
+        due_date = request.POST.get("due_date")
+        Task.objects.create(
+            name=name, 
+            priority=priority,
+            due_date=due_date if due_date else None
+            )
     
     return redirect("index")
 
