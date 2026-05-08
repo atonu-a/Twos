9caeccd154ef2c8e2afbde5546dc84c2eae73676
Atonu <susnigdho1234@gmail.com>
2026-05-09 00:27:37 +0600

    title changed

--- static/js/app.js
+++ static/js/app.js
@@ -12,3 +12,14 @@ window.addEventListener("load", () => {
     loader.classList.add("loader-hidden");
   }, 300);
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const calendarEl = document.getElementById("calendar");
+  const calendar = new FullCalendar.Calendar(calendarEl, {
+    initialView: "dayGridMonth",
+    themeSystem: "standard",
+    events: "/api/get-tasks/", // এই URL থেকে জ্যাংগো ডাটা পাঠাবে
+    eventColor: "#6366f1", // আপনার থিমের Indigo কালার
+  });
+  calendar.render();
+});
